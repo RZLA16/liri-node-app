229e4e65199b8b10c51f9f34231047695aa4ea19
RZLA16 <rstar026@yahoo.com>
2017-07-16 21:43:31 -0700

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_ley:'	JqNkMWQTPSo3Y5YcLifZVEqJv',
+	consumer_secret:'LiqFWXX86g02gjOuVlZEOm3w0OOA11VXSpiRXbMMJKN2YQPot3',
+	access_token_key:'	886710485956952064-plJ1KKzqvcmkgUHra07Q4YzFIqhf4Zu',
+	access_token_secret:'	VVWsleqjy1M1Yc4PvkZHAbQjw7Lf04BcKVG1PWdoF2hYY',
+
+exports.spotifyKeys = {
+	consumer_ley:'	1364ef1e61124deba0dfeae55894c4f4',
+	consumer_secret:'4fff413e4782442bafd7dad35d2ad0f4',
---
+++ liri.js
@@ -0,0 +1,7 @@
+var fs = require("fs");
+var twitter = require('twitter');
+var spotify = require('spotify');
+var ombd= require('omdb');
+var request = require('request');
+var botInput = process.argv[2];
+
